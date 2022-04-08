5486b169a6884e91e5318ea80d96cb66f8293f9b
Dr John Vidler <j.vidler@lancaster.ac.uk>
2022-04-08 10:31:29 +0100

    Added dummy base functions for testing

---
+++ codal-audio.ts
@@ -0,0 +1,36 @@
+/**
+ * Functions to operate the v2 on-board microphone and speaker.
+ */
+//% weight=5 color=#2699BF icon="\uf110"
+namespace codal_audio {
+
+    class V2Audio {
+        /**
+         * Play and recorded audio
+         */
+        //% blockId="codal_audio_r1_play" block="Play Recording"
+        //% weight=85 blockGap=8
+        play() {
+            /* Dummy function */
+        }
+
+        /**
+         * Play and recorded audio
+         */
+        //% blockId="codal_audio_r1_stop" block="Stop"
+        //% weight=85 blockGap=8
+        stop() {
+            /* Dummy function */
+        }
+
+        /**
+         * Play and recorded audio
+         */
+        //% blockId="codal_audio_r1_erase" block="Erase Recording"
+        //% weight=85 blockGap=8
+        erase() {
+            /* Dummy function */
+        }
+
+    }
+}
