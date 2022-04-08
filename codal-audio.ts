e084dbf8b248ec9ad79b61be586c75e7ea02a623
Dr John Vidler <j.vidler@lancaster.ac.uk>
2022-04-08 10:57:37 +0100

    Removed outer class def

--- codal-audio.ts
+++ codal-audio.ts
@@ -4,33 +4,30 @@
 //% weight=5 color=#2699BF icon="\uf110"
 namespace codal_audio {
 
-    class V2Audio {
-        /**
-         * Play and recorded audio
-         */
-        //% blockId="codal_audio_r1_play" block="Play Recording"
-        //% weight=85 blockGap=8
-        play() {
-            /* Dummy function */
-        }
+    /**
+     * Play and recorded audio
+     */
+    //% blockId="codal_audio_r1_play" block="Play Recording"
+    //% weight=85 blockGap=8
+    export function play() {
+        /* Dummy function */
+    }
 
-        /**
-         * Play and recorded audio
-         */
-        //% blockId="codal_audio_r1_stop" block="Stop"
-        //% weight=85 blockGap=8
-        stop() {
-            /* Dummy function */
-        }
+    /**
+     * Play and recorded audio
+     */
+    //% blockId="codal_audio_r1_stop" block="Stop"
+    //% weight=85 blockGap=8
+    export function stop() {
+        /* Dummy function */
+    }
 
-        /**
-         * Play and recorded audio
-         */
-        //% blockId="codal_audio_r1_erase" block="Erase Recording"
-        //% weight=85 blockGap=8
-        erase() {
-            /* Dummy function */
-        }
-
+    /**
+     * Play and recorded audio
+     */
+    //% blockId="codal_audio_r1_erase" block="Erase Recording"
+    //% weight=85 blockGap=8
+    export function erase() {
+        /* Dummy function */
     }
 }
