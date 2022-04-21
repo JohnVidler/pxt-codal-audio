b0e1bfb8c7b9261a52ecec4eeefbb40116035a24
Dr John Vidler <j.vidler@lancaster.ac.uk>
2022-04-21 11:36:09 +0100

    Added the latest copy of the prototype blocks

--- codal-audio.ts
+++ codal-audio.ts
@@ -1,33 +1,103 @@
+enum WaitOpts {
+    Foreground,
+    Background
+}
+
+enum AudioEvent {
+    //% block="Starts Playing"
+    StartedPlaying,
+    //% block="Stops Playing"
+    StoppedPlaying,
+    //% block="Starts Recording"
+    StartedRecording,
+    //% block="Stops Recording"
+    StoppedRecording
+}
+
+enum SampleRateScope {
+    Everything,
+    Playback,
+    Recording
+}
+
 /**
  * Functions to operate the v2 on-board microphone and speaker.
  */
-//% weight=5 color=#2699BF icon="\uf6a8" block="V2 Audio Blocks" advanced=false
+//% weight=5 color=#e26fb4 icon="\uf130" block="V2 Audio" advanced=false
 namespace codalAudio {
 
     /**
-     * Play and recorded audio
+     * Record an audio clip
+     * 
+     * @param sync If true, block until we run out of memory!
      */
-    //% blockId="codal_audio_r1_play" block="Play Recording"
-    //% weight=85 blockGap=8
-    export function play() {
+    //% block="​Start recording || and record for %time seconds"
+    //% expandableArgumentMode="enabled"
+    //% time.defl=3
+    export function record( time?: number ) : void {
+        /* Dummy function */
+    }
+
+    /**
+     * Set the sample rate for recording, playback, or both
+     * 
+     * @param hz The sample rate, in Hz
+     */
+    //% block="Set sample rate to %hz Hz || for %scope"
+    //% expandableArgumentMode="enabled"
+    //% hz.defl=11000
+    export function setSampleRate( hz?: number, scope?: SampleRateScope ) : void {
+        /* Dummy function */
+    }
+
+    /**
+     * Play any recorded audio
+     * 
+     * @param sync If true, block until complete
+     */
+    //% block="​Start playback || in the %sync and play for %time seconds"
+    //% expandableArgumentMode="enabled"
+    //% sync.defl=true
+    //% time.defl=3
+    export function play( sync?: WaitOpts, time?: number ) : void {
         /* Dummy function */
     }
 
     /**
      * Play and recorded audio
      */
-    //% blockId="codal_audio_r1_stop" block="Stop"
-    //% weight=85 blockGap=8
-    export function stop() {
+    //% block="Stop"
+    export function stop() : void {
         /* Dummy function */
     }
 
     /**
      * Play and recorded audio
      */
-    //% blockId="codal_audio_r1_erase" block="Erase Recording"
-    //% weight=85 blockGap=8
-    export function erase() {
+    //% block="Erase Recording"
+    export function erase() : void {
         /* Dummy function */
     }
+
+    //% block
+    export function soundIsPlaying() : boolean {
+        return false;
+    }
+
+    //% block
+    export function soundIsRecording() : boolean {
+        return false;
+    }
+
+    //% block
+    export function soundIsStopped() : boolean {
+        return false;
+    }
+
+    //% block="When audio %eventType"
+    export function audioEvent( eventType: AudioEvent, handler: () => void ) : void {
+        /* Dummy function */
+    }
+
+    
 }
