79f7ffd782cc370f1f934446a3911ab99b5230f8
Dr John Vidler <j.vidler@lancaster.ac.uk>
2022-05-17 13:29:14 +0100

    Added sample rate shims and more CPP support

--- codal-audio.cpp
+++ codal-audio.cpp
@@ -8,6 +8,16 @@ namespace codalAudio {
 
     static StreamRecording * recording = NULL;
 
+    void enableMic() {
+        uBit.audio.activateMic();
+        uBit.audio.mic->enable();
+    }
+
+    void disableMic() {
+        uBit.audio.mic->disable();
+        uBit.audio.deactivateMic();
+    }
+
     void checkEnv() {
         if( recording == NULL ) {
             recording = new StreamRecording( *uBit.audio.splitter );
@@ -15,8 +25,7 @@ namespace codalAudio {
             MixerChannel * channel = uBit.audio.mixer.addChannel( *recording, CHANNEL_FREQ );
 
             // By connecting to the mic channel, we activate it automatically, so shut it down again.
-            uBit.audio.deactivateMic();
-            uBit.audio.mic->disable();
+            
 
             channel->setVolume( 100.0 );
             uBit.audio.mixer.setVolume( 1000 );
@@ -27,12 +36,47 @@ namespace codalAudio {
     //%
     void record() {
         codalAudio::checkEnv();
+        enableMic();
         recording->record();
+    }
+
+    //%
+    void play() {
+        codalAudio::checkEnv();
+        enableMic();
+        recording->play();
     }
 
     //%
     void stop() {
         codalAudio::checkEnv();
         recording->stop();
+        disableMic();
+    }
+
+    //%
+    void erase() {
+        codalAudio::checkEnv();
+        recording->erase();
+    }
+
+    //%
+    int audioDuration( int sampleRate ) {
+        return recording->duration( sampleRate )
+    }
+
+    //%
+    bool audioIsPlaying() {
+        return recording->isPlaying();
+    }
+
+    //%
+    bool audioIsRecording() {
+        return recording->isRecording();
+    }
+
+    //%
+    bool audioIsStopped() {
+        return recording->isStopped();
     }
 }
--- codal-audio.ts
+++ codal-audio.ts
@@ -27,15 +27,30 @@ enum AudioGainEnum {
 //% weight=5 color=#e26fb4 icon="\uf130" block="V2 Audio" advanced=false
 namespace codalAudio {
 
+    // Shim state
+    let _recordingFreqHz: number        = 22000
+    let _playbackFreqHz:  number        = 22000
+    let _micGain:         AudioGainEnum = AudioGainEnum.Medium
+
+    /*let _memoryFill:      number        = 0
+
+    setInterval( () => {
+        if( _memoryFill < 100 )
+            _memoryFill++
+        console.log( _memoryFill )
+    }, 100 )*/
+
     /**
      * Record an audio clip
      * 
      * @param sync If true, block until we run out of memory!
      */
     //% block="start recording"
-    //% him=codalAudio::record
+    //% shim=codalAudio::record
     export function record() : void {
         /* Dummy function */
+        console.log( "CodalAudio -> record()" )
+        return
     }
 
     /**
@@ -46,8 +61,22 @@ namespace codalAudio {
     //% block="set sample rate to %hz Hz || for %scope"
     //% expandableArgumentMode="enabled"
     //% hz.defl=11000
-    export function setSampleRate( hz?: number, scope?: AudioSampleRateScope ) : void {
-        /* Dummy function */
+    export function setSampleRate( hz: number, scope?: AudioSampleRateScope ) : void {
+        switch( scope )
+        {
+            case AudioSampleRateScope.Everything:
+                _recordingFreqHz = hz;
+                _playbackFreqHz = hz;
+                break;
+            
+            case AudioSampleRateScope.Playback:
+                _playbackFreqHz = hz;
+                break;
+            
+            case AudioSampleRateScope.Recording:
+                _recordingFreqHz = hz;
+                break;
+        }
     }
 
     /**
@@ -57,8 +86,10 @@ namespace codalAudio {
      */
     //% block="set microphone gain to %gain"
     //% gain.defl=Medium
-    export function setMicrophoneGain( gain?: AudioGainEnum ) : void {
+    export function setMicrophoneGain( gain: AudioGainEnum ) : void {
         /* Dummy function */
+        _micGain = gain;
+        return
     }
 
     /**
@@ -67,8 +98,11 @@ namespace codalAudio {
      * @param sync If true, block until complete
      */
     //% block="​start playback"
+    //% shim=codalAudio::play
     export function play() : void {
         /* Dummy function */
+        console.log( "CodalAudio -> play()" )
+        return
     }
 
     /**
@@ -78,27 +112,56 @@ namespace codalAudio {
     //% shim=codalAudio::stop
     export function stop() : void {
         /* Dummy function */
+        console.log( "CodalAudio -> stop()" )
+        return
     }
 
     /**
      * Play and recorded audio
      */
     //% block="erase recording"
+    //% shim=codalAudio::erase
     export function erase() : void {
         /* Dummy function */
+        console.log( "CodalAudio -> erase()" )
+        return
+    }
+
+    //% block="microphone gain"
+    export function micGain() : AudioGainEnum {
+        return _micGain;
+    }
+
+    //% block="recording frequency"
+    export function recordingHz() : number {
+        return _recordingFreqHz;
+    }
+
+    //% block="playback frequency"
+    export function playbackHz() : number {
+        return _playbackFreqHz;
+    }
+
+    //% block="audio duration at %hz hz"
+    //% shim=codalAudio::audioDiration
+    export function audioDuration( hz?: number ) : number {
+        return 3;
     }
 
     //% block="audio is playing"
+    //% shim=codalAudio::audioIsPlaying
     export function audioIsPlaying() : boolean {
         return false;
     }
 
     //% block="audio is recording"
+    //% shim=codalAudio::audioIsRecording
     export function audioIsRecording() : boolean {
         return false;
     }
 
     //% block="audio is stopped"
+    //% shim=codalAudio::audioIsStopped
     export function audioIsStopped() : boolean {
         return false;
     }
@@ -106,6 +169,8 @@ namespace codalAudio {
     //% block="when audio %eventType"
     export function audioEvent( eventType: AudioEvent, handler: () => void ) : void {
         /* Dummy function */
+        console.log( "CodalAudio -> audioEvent()" )
+        return
     }
 
     
